06922d6199202238c64826ccb967d660f9b617c5
Bob-development <proinvoke228@gmail.com>
2023-10-27 19:11:17 +0300

    improve index by added houses

--- schemas/Property/index.js
+++ schemas/Property/index.js
@@ -1,11 +1,14 @@
 //HOUSES
 import { House } from "./Houses/BaseHouse/House";
+import { NightCityHouse } from "./Houses/NightCIty_House/NightCityHouse";
+import { OsakaHouse } from "./Houses/Osaka_House/OsakaHouse";
+import { SeattleHouse } from "./Houses/Seattle_House/SeattleHouse";
+import { TychoHouse } from "./Houses/Tycho_House/TychoCity";
 
 //CARS
 import { Car } from "./Cars/BaseCar/Car";
 import { ExpensiveCar } from "./Cars/ExpensiveCar/ExpensiveCar";
 import { CheapCar } from "./Cars/CheapCar/CheapCar";
-import { NightCityHouse } from "./Houses/NightCIty_House/NightCityHouse";
 
 
-export {House, NightCityHouse, Car, ExpensiveCar, CheapCar};
+export {House, NightCityHouse, OsakaHouse, SeattleHouse, TychoHouse, Car, ExpensiveCar, CheapCar};
